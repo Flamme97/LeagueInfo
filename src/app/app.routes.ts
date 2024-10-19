b4a254451ad301bfe8f25c7439b1aa2d7aa95a2e
obl <obl@danalock.com>
2024-10-19 17:13:06 +0200

    more basics setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'search', component: UserinfoComponent,
+  }
+
+
+
+];
