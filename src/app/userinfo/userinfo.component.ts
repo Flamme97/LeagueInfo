1ff7af4d5463a4a1a015e1b4810ea1969821e617
obl <obl@danalock.com>
2024-10-19 17:13:13 +0200

    more basics

--- src/app/userinfo/userinfo.component.ts
+++ src/app/userinfo/userinfo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-userinfo',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './userinfo.component.html',
   styleUrl: './userinfo.component.scss'
 })
